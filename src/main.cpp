b12c8f8de254e5ddb25d284c4b8683930f6c0cc2
SDf-SD <nisitt321@gmail.com>
2025-10-30 16:36:16 -0400

    Updated main into command suite with new functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,43 @@
-#include "Job.h"
-#include "FCFSScheduler.h"
 #include <iostream>
-#include <vector>
 
-int main() {
-    int simTime = 1000000;
-    std::vector<Job> allJobs = generateJobs(simTime);
+#include "Scheduler.h"
+#include "FCFSScheduler.h"
+#include "PriorityArrayScheduler.h"
+#include "Job.h"
+
+void helpPrompt() {
+    std::cout << "Welcome to rtosSuite - this is a program to compare different scheduling algorithms for Real Time Operating Systems\n"
+                 "Commands:\n"
+                 "\t-compare {x} {y} - \n"
+                 "Flags:\n"
+                 "\t--render - signals that a window will be opened to visualize the completion and processing of jobs.\n"
+                 "\t--help - shows this\n"
+                 "\t--simTime {value: int} - modifies the default simulation time to desired value\n";
+}
+
+enum CommandType {
+    NONE = -1,
+    COMPARE = 0,
+};
+
+struct Command {
+    CommandType typ;
+    union {
+        struct { int x, y; } compare;
+    } args;
+};
+
+struct Settings {
+    int simulationTime = 1000000;
+    bool render = false;
+
+    Command command = Command { .typ = NONE };
+};
+
+void simulate(Scheduler &scheduler, std::vector<Job> allJobs, int simTime) {
     int nextJobIdx = 0;
     std::vector<Job> completedJobs;
 
-    FCFSScheduler scheduler;
     Job* runningJob = nullptr;
     
     for (int t = 0; t <= simTime; t++) {
@@ -47,5 +75,62 @@ int main() {
         }
     }
     
-    return 0;
+}
+
+int main(int argc, char **argv) {
+    Settings settings;
+
+    if (argc == 1) {
+        helpPrompt();
+        return 0;
+    }
+
+    bool commandFound = false;
+
+    for (int i = 1; i < argc; i++) { // ignore first arg (executable name)
+        if (!commandFound) {
+            if (strncmp(argv[i], "-compare", 9) == 0) {
+                if (i + 2 >= argc) return -1;
+
+                // TODO: Sanitize inputs if two ints aren't supplied
+
+                settings.command = Command { .typ = CommandType::COMPARE, .args.compare = { .x = std::atoi(argv[i + 1]), .y = std::atoi(argv[i + 2])}};
+            }
+        }
+
+        if (strncmp(argv[i], "--help", 7) == 0) {
+            helpPrompt();
+            return 0;
+        } else if(strncmp(argv[i], "--simTime", 10) == 0) {
+            if (i + 1 >= argc) return -1;
+            int simTime = std::atoi(argv[i + 1]);
+
+            settings.simulationTime = simTime;
+
+            i++;
+        }
+    }
+    
+    switch (settings.command.typ)
+    {
+    case CommandType::NONE:
+        break;
+    case CommandType::COMPARE:
+    {
+        std::cout << "Comparing schedulers...\n";
+        std::vector<Job> allJobs = generateJobs(settings.simulationTime);
+
+        // use command arguments to decide which schedulers to use.
+
+        FCFSScheduler fcfs;
+        PriorityArrayScheduler pa; // TODO: FIX THIS currently segfaults
+        simulate(fcfs, allJobs, settings.simulationTime);
+        // simulate(pa, allJobs);
+        break;
+    }
+    default:
+        break;
+    }
+
+    return 0;   
 }
